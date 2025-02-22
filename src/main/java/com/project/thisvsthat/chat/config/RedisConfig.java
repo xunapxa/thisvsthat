654fe30a9b6ae1e7133e040a8691fc209caaffe8
sun <seon04520@gmail.com>
2025-02-23 01:21:32 +0900

    Fix: 레디스 서버 설정 변경

--- src/main/java/com/project/thisvsthat/chat/config/RedisConfig.java
+++ src/main/java/com/project/thisvsthat/chat/config/RedisConfig.java
@@ -1,10 +1,13 @@
 package com.project.thisvsthat.chat.config;
 
+import com.project.thisvsthat.chat.util.RedisSubscriber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -21,9 +24,17 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int redisPort;
 
+    @Value("${spring.data.redis.password}")
+    private String redisPassword;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisHost, redisPort);
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisHost);
+        config.setPort(redisPort);
+        config.setPassword(RedisPassword.of(redisPassword));
+
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
@@ -40,19 +51,19 @@ public class RedisConfig {
         return new ChannelTopic("chatroom");
     }
 
-//    @Bean
-//    public MessageListenerAdapter messageListener(RedisSubscriber subscriber) {
-//        return new MessageListenerAdapter(subscriber, "onMessage");
-//    }
-//
-//    @Bean
-//    public RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory,
-//                                                        RedisSubscriber subscriber,
-//                                                        MessageListenerAdapter messageListener,
-//                                                        ChannelTopic topic) {
-//        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-//        container.setConnectionFactory(connectionFactory);
-//        container.addMessageListener(messageListener, topic);
-//        return container;
-//    }
+    @Bean
+    public MessageListenerAdapter messageListener(RedisSubscriber subscriber) {
+        return new MessageListenerAdapter(subscriber, "onMessage");
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory,
+                                                        RedisSubscriber subscriber,
+                                                        MessageListenerAdapter messageListener,
+                                                        ChannelTopic topic) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(messageListener, topic);
+        return container;
+    }
 }
