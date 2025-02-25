5b3683266326f640c03f08927e296f7737414c8c
sun <seon04520@gmail.com>
2025-02-26 03:24:00 +0900

    Revert "Chore: 제목 너비 동적으로 변경, 스크롤 메서드 변경된 이름으로 적용"
    This reverts commit 277f32a44af49bbdd69282502b68ac28fdc6e22b.

--- src/main/resources/static/js/chat/chat-room.js
+++ src/main/resources/static/js/chat/chat-room.js
@@ -291,7 +291,7 @@ messageInput.on('input change', function () {
                     </div>
                 `);
             }
-            keepScrollAtBottom();
+            scrollToBottom();
         }, function(error) {
             console.error('구독 오류:', error);
         });
